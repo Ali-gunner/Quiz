c9f242dfd8b32426e1d463935f7913ac076a88bf
Ali-gunner <ali.ranjbar.lut2024@gmail.com>
2025-12-15 10:10:00 +0200

    moving to next question is handled

---
+++ src/components/NextButton.jsx
@@ -0,0 +1,38 @@
+//import { useQuiz } from "../contexts/QuizContext";
+
+function NextButton({dispatch, answer}) {
+  //const { dispatch, answer, index, numQuestions } = useQuiz();
+
+  if (answer === null) return null;
+
+  return (
+      <button
+        className="btn btn-ui"
+        onClick={() => dispatch({ type: "nextQuestion" })}
+      >
+        Next
+      </button>
+    );
+
+  // if (index < numQuestions - 1)
+  //   return (
+  //     <button
+  //       className="btn btn-ui"
+  //       onClick={() => dispatch({ type: "nextQuestion" })}
+  //     >
+  //       Next
+  //     </button>
+  //   );
+
+  // if (index === numQuestions - 1)
+  //   return (
+  //     <button
+  //       className="btn btn-ui"
+  //       onClick={() => dispatch({ type: "finish" })}
+  //     >
+  //       Finish
+  //     </button>
+  //   );
+}
+
+export default NextButton;
